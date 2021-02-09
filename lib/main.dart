ab6fe77575045f2b691a847ccfcc477659ad4949
Maksym <maksym.sh@upplabs.com>
2021-02-09 15:50:04 +0200

    intl-package

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 import './transaction.dart';
 
@@ -86,7 +87,7 @@ class MyHomePage extends StatelessWidget {
                           ),
                         ),
                         Text(
-                          elem.date.toString(),
+                          DateFormat.yMMMd().format(elem.date),
                           style: TextStyle(
                             color: Colors.grey,
                           ),
